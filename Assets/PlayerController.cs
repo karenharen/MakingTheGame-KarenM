cbc9caa47153859e6b1814278a1a1d69a2f1dae6
Karen <photo.karen@gmail.com>
2020-03-23 11:41:08 -0700

    classwork

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRB;
+    public float ForceMultiplier;
+    public float gravityMultiplier;
+    public bool onGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMultiplier;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            playerRB.AddForce(Vector3.up * ForceMultiplier, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+    }
+}
---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnLocaiton = new Vector3(30, 0, 0);
+    private float initalWait = 1;
+    private float periodRepeat = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", initalWait, periodRepeat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnLocaiton, obstaclePrefab.transform.rotation);
+    }
+}
